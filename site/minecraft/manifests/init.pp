84c94ea4a3a8032c207e2dd5119401afd3a1f4e0
Code Duet <info@codeduet.com>
2019-03-27 16:22:27 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -5,6 +5,7 @@ class minecraft {
   file {‘/opt/minecraft/server.jar’:
     ensure => file,
     source      => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+    before => Service['minecraft'],
   }
   package {‘java’:
     ensure => present,
