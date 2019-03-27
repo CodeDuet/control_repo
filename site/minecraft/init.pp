4a9a00229045bc32fe2d5c3740f2431068cdcc2e
Code Duet <info@codeduet.com>
2019-03-27 13:17:43 -0700

    Update init.pp

--- site/minecraft/init.pp
+++ site/minecraft/init.pp
@@ -1,24 +1,24 @@
 class minecraft {
-  file{'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_server.jar':
+  file {‘/opt/minecraft/server.jar’:
     ensure => file,
-    source => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar'
+    source      => 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
   }
-  package {'java':
-    ensure => present
+  package {‘java’:
+    ensure => present,
   }
-  file {'/opt/minecraft/eula.txt'
+  file {‘/opt/minecraft/eula.txt’:
     ensure => file,
-    content => 'eula=true'
+    content => ‘eula=true’
   }
-  file {'/etc/systemd/system/minecraft.server':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service'
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
   }
-  service {'minecraft':
+  service { ‘minecraft’:
     ensure => running,
-    enable => true
+    enable => true,
   }
 }
