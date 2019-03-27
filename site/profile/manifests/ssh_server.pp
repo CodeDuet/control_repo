5cf9bf19e88ff50bab6333e77c8201619de68d32
Code Duet <info@codeduet.com>
2019-03-27 11:48:29 -0700

    Update ssh_server.pp

--- site/profile/manifests/ssh_server.pp
+++ site/profile/manifests/ssh_server.pp
@@ -10,6 +10,6 @@ class profile::ssh_server {
 		ensure => present,
 		user   => 'root',
 		type   => 'ssh-rsa',
-		key    => '',
+		key    => 'AAAAB3NzaC1yc2EAAAADAQABAAABAQC0+Yh1TPvj9buB4o6JvvkzPk+BcVCY7IqxxX1s2NdynYUL/nO/zZfd+TGAV9cevi9iSKfL/O47BquCCzE+xomZRBQ0JVFUszLP+yLj75NE+djrq0RlJes9dzqMXqr7CrNbM7KxCZBUs3xxfGDe3k8eaFP0six27Jfvmp1J+FtH7SlHXnO3uZygwFxpBXdPMlmRUFsRBIYQfnMguTEQWdgp26OuYUX9VnY374ifdm36i+pEma+/l1/5NFep5aCtp16x/fjTw+caqEGgTsOCSdD6d0+n5FDyZj1HjI9viqNQdYajYT5umV0r7KaRgRw3r/LcqqLywgzULXW2uIxeYU7L',
 	}  
 }
