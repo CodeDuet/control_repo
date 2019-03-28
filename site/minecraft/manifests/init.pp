8de4bcbf9eb496788141c78a49f6d1bf3486b5ed
Code Duet <info@codeduet.com>
2019-03-28 01:53:33 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -14,4 +14,12 @@ class minecraft (
     replace => 'yes',
     content => "eula=true",
     }
+    file {'/etc/systemd/system/minecraft.service':
+       ensure => file,
+       source => 'puppet:///modules/minecraft/minecraft.service',
+  }
+    service { 'minecraft':
+      ensure => running,
+      enable => true,
+  }
 }
