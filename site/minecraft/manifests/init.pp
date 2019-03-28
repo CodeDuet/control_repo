51f142842914a8f25326e929da049a5be4aaa0c7
Code Duet <info@codeduet.com>
2019-03-28 01:41:25 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -2,7 +2,6 @@ class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
   $install_dir = "/opt/minecraft"
   ) {
-    ensure_packages('java')
     file {"${install_dir}":
         ensure => directory,
     }
