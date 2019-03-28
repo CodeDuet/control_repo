3db411fe4489690167618befde9ce85648d03e30
Code Duet <info@codeduet.com>
2019-03-27 17:17:26 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -10,9 +10,6 @@ class minecraft (
     source      => $url,
     before => Service['minecraft'],
   }
-  package {‘java’:
-    ensure => installed,
-  }
   file {"${install_dir}/eula.txt":
     ensure => file,
     content => ‘eula=true’
@@ -24,6 +21,6 @@ class minecraft (
   service { ‘minecraft’:
     ensure => running,
     enable => true,
-    require => [Package ['java'],File["${install_dir}/eula.txt"],File[‘/etc/systemd/system/minecraft.service’]],
+    require => [File["${install_dir}/eula.txt"],File[‘/etc/systemd/system/minecraft.service’]],
   }
 }
