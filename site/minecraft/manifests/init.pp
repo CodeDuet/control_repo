bc6d391501ef36e85969644182684c52b9d6c9d9
Code Duet <info@codeduet.com>
2019-03-28 01:10:57 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,2 +1,18 @@
-class minecraft {
+class minecraft (
+  $url = 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
+  $install_dir = "/opt/minecraft"
+  ) {
+    
+    file {$install_dir:
+        ensure => directory,
+    }
+    file {"${install_dir}/server.jar":
+        ensure => file,
+        source => $url,
+        before => Service['minecraft'],
+    }
+    file {"${install_dir}/eula.txt":
+        ensure => file,
+        content => ‘eula=true’
+    }
 }
