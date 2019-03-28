b46eb434d165f5264f85010a152c4d3df3c28d86
Code Duet <info@codeduet.com>
2019-03-28 00:49:02 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,14 +1,22 @@
-class minecraft (
+class minecraft::install (
   $url = 'https://launcher.mojang.com/v1/objects/3737db93722a9e39eeada7c27e7aca28b144ffa7/server.jar',
   $install_dir = "/opt/minecraft"
   ){
-  file {"${install_dir}":
+  package {‘java’:
+    require => Exec['yum update -y'],
+    ensure => installed,
+  }
+  file {$install_dir:
     ensure => directory,
   }
   file {"${install_dir}/server.jar":
     ensure => file,
-    source      => $url,
+    source => $url,
     before => Service['minecraft'],
+  }
+  file {"${install_dir}/eula.txt":
+    ensure => file,
+    content => ‘eula=true’
   }
   file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
@@ -18,9 +26,5 @@ class minecraft (
     ensure => running,
     enable => true,
     require => [File["${install_dir}/eula.txt"],File[‘/etc/systemd/system/minecraft.service’]],
-  }
-  file {"${install_dir}/eula.txt":
-    ensure => file,
-    content => ‘eula=true’
-  }
+  } 
 }
